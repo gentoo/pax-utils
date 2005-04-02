b0d605b403467205df2d662ccde2725cc2037d80
Mike Frysinger <vapier@gentoo.org>
2005-04-02 00:13:44 +0000

    make really pretty

--- pspax.c
+++ pspax.c
@@ -19,7 +19,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+#include <libgen.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <ctype.h>
@@ -40,167 +40,160 @@
 
 #define PROC_DIR "/proc"
 
-extern char *basename();
-
-char *get_proc_name(pid_t pid)
+static char *get_proc_name(pid_t pid)
 {
-   FILE *fp;
-   char buf[PATH_MAX];
-   static char *p = "-----";
-   memset(&buf, 0, sizeof(buf));
+	FILE *fp;
+	char buf[PATH_MAX];
+	static char *p = "-----";
+	memset(&buf, 0, sizeof(buf));
 
-   snprintf(buf, sizeof(buf), "/proc/%d/stat", (int) pid);
+	snprintf(buf, sizeof(buf), PROC_DIR "/%d/stat", (int) pid);
 
-   fp = fopen(buf, "r");
+	fp = fopen(buf, "r");
 
-   if (fp == NULL)
-      return p;
+	if (fp == NULL)
+		return p;
 
-   fscanf(fp, "%*d %s.16", buf);
+	fscanf(fp, "%*d %s.16", buf);
 
-   if (*buf) {
-      buf[strlen(buf) - 1] = '\0';
-      buf[16] = 0;
-      p = buf;
-      *p++;
-   }
-   fclose(fp);
-   return p;
+	if (*buf) {
+		buf[strlen(buf) - 1] = '\0';
+		buf[16] = 0;
+		p = buf;
+		*p++;
+	}
+	fclose(fp);
+	return p;
 }
 
-struct passwd *get_proc_uid(pid_t pid)
+static struct passwd *get_proc_uid(pid_t pid)
 {
-   struct passwd *pwd;
-   struct stat st;
-   static char s[PATH_MAX];
+	struct passwd *pwd;
+	struct stat st;
+	static char s[PATH_MAX];
 
-   snprintf(s, sizeof(s), "/proc/%d/stat", (int) pid);
-
-   if ((stat(s, &st)) != (-1)) {
-      if ((pwd = getpwuid(st.st_uid)) != NULL)
-	 return pwd;
-   }
-   return NULL;
+	snprintf(s, sizeof(s), PROC_DIR "/%d/stat", (int) pid);
+	if ((stat(s, &st)) != (-1))
+		if ((pwd = getpwuid(st.st_uid)) != NULL)
+			return pwd;
+	return NULL;
 }
 
 static char *get_proc_status(pid_t pid, char *name)
 {
-   FILE *fp;
-   int len;
-   static char s[PATH_MAX];
+	FILE *fp;
+	int len;
+	static char s[PATH_MAX];
 
-   snprintf(s, sizeof(s), "/proc/%d/status", (int) pid);
-   if ((fp = fopen(s, "r")) == NULL)
-      return NULL;
+	snprintf(s, sizeof(s), PROC_DIR "/%d/status", (int) pid);
+	if ((fp = fopen(s, "r")) == NULL)
+		return NULL;
 
-   len = strlen(name);
-   while (fgets(s, sizeof(s), fp)) {
-      if (strncasecmp(s, name, len) == 0) {
-	 if (s[len] == ':') {
-	    fclose(fp);
-	    s[strlen(s) - 1] = 0;
-	    return (s + len + 2);
-	 }
-      }
-   }
-   fclose(fp);
-   return NULL;
+	len = strlen(name);
+	while (fgets(s, sizeof(s), fp)) {
+		if (strncasecmp(s, name, len) == 0) {
+			if (s[len] == ':') {
+				fclose(fp);
+				s[strlen(s) - 1] = 0;
+				return (s + len + 2);
+			}
+		}
+	}
+	fclose(fp);
+	return NULL;
 }
 
 static char *get_pid_attr(int pid)
 {
-   FILE *fp;
-   char *p;
-   char s[32];
-   static char buf[BUFSIZ];
-   memset(buf, 0, sizeof(buf));
-   snprintf(s, sizeof(s), "/proc/%d/attr/current", pid);
-   if ((fp = fopen(s, "r")) == NULL)
-      return "-";
-   if (fgets(buf, sizeof(buf), fp) != NULL)
-      if ((p = strchr(buf, '\n')) != NULL)
-	 *p = 0;
-   fclose(fp);
-   return (char *) buf;
+	FILE *fp;
+	char *p;
+	char s[32];
+	static char buf[BUFSIZ];
+	memset(buf, 0, sizeof(buf));
+	snprintf(s, sizeof(s), PROC_DIR "/%d/attr/current", pid);
+	if ((fp = fopen(s, "r")) == NULL)
+		return "-";
+	if (fgets(buf, sizeof(buf), fp) != NULL)
+		if ((p = strchr(buf, '\n')) != NULL)
+			*p = 0;
+	fclose(fp);
+	return buf;
 }
 
-static char *get_pid_type(int pid)
+static const char *get_pid_type(int pid)
 {
-   char fname[32];
-   elfobj *elf = NULL;
-   static char *ret;
+	char fname[32];
+	elfobj *elf = NULL;
+	char *ret = NULL;
 
-   ret = "-------";
-
-   snprintf(fname, sizeof(fname), "/proc/%d/exe", pid);
-   if ((elf = readelf(fname)) == NULL)
-      return ret;
-   if (!check_elf_header(elf->ehdr))
-      if (IS_ELF(elf))
-	 ret = (char *) get_elfetype(elf->ehdr->e_type);
-   munmap(elf->data, elf->len);
-   free(elf);
-   return ret;
+	snprintf(fname, sizeof(fname), PROC_DIR "/%d/exe", pid);
+	if ((elf = readelf(fname)) == NULL)
+		return ret;
+	if (!check_elf_header(elf->ehdr) && IS_ELF(elf))
+		ret = (char *)get_elfetype(elf->ehdr->e_type);
+	unreadelf(elf);
+	return ret;
 }
 
-void pspax()
+static void pspax()
 {
-   register DIR *dir;
-   register struct dirent *de;
-   pid_t pid;
-   struct passwd *pwd;
-   struct stat st;
-   char *p, *result;
-
+	register DIR *dir;
+	register struct dirent *de;
+	pid_t pid;
+	struct passwd *pwd;
+	struct stat st;
+	char *p, *result, *pid_type;
 #ifdef WANT_SYSCAP
-   ssize_t length;
-   cap_t cap_d;
-   cap_d = cap_init();
+	ssize_t length;
+	cap_t cap_d;
+	cap_d = cap_init();
 #endif
 
-   chdir(PROC_DIR);
-   if (!(dir = opendir(PROC_DIR))) {
-      perror(PROC_DIR);
-      exit(EXIT_FAILURE);
-   }
-   fprintf(stdout, "%8s %-6s %-6s %-10s %-16s %s\n",
-	   "USER", "PID", "PAX", "ELF_TYPE", "NAME", "CAPS");
+	chdir(PROC_DIR);
+	if (!(dir = opendir(PROC_DIR))) {
+		perror(PROC_DIR);
+		exit(EXIT_FAILURE);
+	}
+	printf("%8s %-6s %-6s %-10s %-16s %s\n",
+	       "USER", "PID", "PAX", "ELF_TYPE", "NAME", "CAPS");
 
-   while ((de = readdir(dir))) {
-      errno = 0;
-      stat(de->d_name, &st);
-      if ((errno != ENOENT) && (errno != EACCES)) {
-	 pid = (pid_t) atoi((char *) basename((char *) de->d_name));
-	 if (!pid)
-	    continue;
-	 {
+	while ((de = readdir(dir))) {
+		errno = 0;
+		stat(de->d_name, &st);
+		if ((errno != ENOENT) && (errno != EACCES)) {
+			pid = (pid_t) atoi((char *) basename((char *) de->d_name));
+			if (!pid)
+				continue;
+
 #ifdef WANT_SYSCAP
-	    /* this is a non-POSIX function */
-	    capgetp(pid, cap_d);
-	    result = cap_to_text(cap_d, &length);
+			/* this is a non-POSIX function */
+			capgetp(pid, cap_d);
+			result = cap_to_text(cap_d, &length);
 #else
-	    result = "=";
+			result = "=";
 #endif
-	    pwd = get_proc_uid(pid);
-	    fprintf(stdout, "%-8s %-6d %-6s %-10s %-16s %s %s\n",
-		    (pwd != NULL) ? pwd->pw_name : "-----",
-		    pid,
-		    (p =
-		     get_proc_status(pid, "PAX")) ? p : "------",
-		    get_pid_type(pid), get_proc_name(pid), result,
-		    get_pid_attr(pid));
+			pwd = get_proc_uid(pid);
+			pid_type = (char *)get_pid_type(pid);
+			if (pid_type) /* hide kernel processses */
+				printf("%-8s %-6d %-6s %-10s %-16s %s %s\n",
+				       (pwd != NULL) ? pwd->pw_name : "----",
+				       pid,
+				       (p = get_proc_status(pid, "PAX")) ? p : "------",
+				       pid_type,
+				       get_proc_name(pid),
+				       result,
+				       get_pid_attr(pid));
 #ifdef WANT_SYSCAP
-	    if (result)
-	       cap_free(result);
+			if (result)
+				cap_free(result);
 #endif
-	 }
-      }
-   }
-   closedir(dir);
+		}
+	}
+	closedir(dir);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-   pspax();
-   return 0;
+	pspax();
+	return EXIT_SUCCESS;
 }
