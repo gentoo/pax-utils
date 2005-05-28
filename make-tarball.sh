61e98b2e56e11d174be371dd4b5327efb19d5193
Mike Frysinger <vapier@gentoo.org>
2005-05-28 06:35:34 +0000

    make tarball generation simple

---
+++ make-tarball.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]] ; then
+	echo "Usage: $0 <ver>" 1>&2
+	exit 1
+fi
+
+ver=$1
+bn=pax-utils-${ver}
+[[ -d ${bn} ]] && rm -r ${bn}
+mkdir ${bn} || exit 1
+cp -r Makefile README *.[ch] man ${bn}/ || exit 1
+tar -jcf ${bn}.tar.bz2 ${bn} || exit 1
+rm -r ${bn} || exit 1
+du -b ${bn}.tar.bz2
