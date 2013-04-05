aa92451d1a40835986449321023dacd4762cfaf2
Mike Frysinger <vapier@gentoo.org>
2013-04-05 22:04:41 +0000

    lddtree: update copyright lines

--- lddtree.sh
+++ lddtree.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Copyright 2007-2012 Gentoo Foundation
-# Copyright 2007-2012 Mike Frysinger <vapier@gentoo.org>
+# Copyright 2007-2013 Gentoo Foundation
+# Copyright 2007-2013 Mike Frysinger <vapier@gentoo.org>
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-projects/pax-utils/lddtree.sh,v 1.19 2013/01/22 02:12:35 vapier Exp $
+# $Header: /var/cvsroot/gentoo-projects/pax-utils/lddtree.sh,v 1.20 2013/04/05 22:04:41 vapier Exp $
 
 argv0=${0##*/}
 
@@ -28,7 +28,7 @@ usage() {
 }
 
 version() {
-	local id='$Id: lddtree.sh,v 1.19 2013/01/22 02:12:35 vapier Exp $'
+	local id='$Id: lddtree.sh,v 1.20 2013/04/05 22:04:41 vapier Exp $'
 	id=${id##*,v }
 	exec echo "lddtree-${id% * Exp*}"
 }
